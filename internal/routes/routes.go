0c608e2dc5fc2a4caf6a554078a213fbb53a728a
Jesse McKenzie <jesse@jessemckenzie.com>
2025-08-22 16:16:03 -0700

    Add search page, navbar, and static assets
    Introduces a new search page with a search bar component, updates routing and handlers to support search, and adds a navbar partial. Includes new CSS files for base styles, components, home, and search pages, as well as static assets like images and icons. Refactors template structure for improved layout and navigation.

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"gin/internal/handlers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +11,16 @@ func Register(router *gin.Engine) {
 	// Load templates folder
 	router.LoadHTMLGlob("internal/templates/**/*")
 
+	// Serve static files
+	router.Static("/static", "./internal/static")
+
 	// Public routes
 	router.GET("/", handlers.Home)
-	router.GET("/blog", handlers.Blog)
+	router.GET("/search", handlers.Search)
+
+	// Partial routes
+	router.POST("/api/search", func(c *gin.Context) {
+		value := c.PostForm("search")
+		c.String(http.StatusOK, "<h1>This is a test data thing! %s</h1>", value)
+	})
 }
